8be345e10362fafb654875d0accb1a66b6db0b16
Steven Holdsworth <sholdsworth@gmail.com>
2023-02-08 20:33:32 +0000

    Simhashes 🐇🐢

---
+++ lib/simhash_bench.ex
@@ -0,0 +1,18 @@
+defmodule SimhashBench do
+  @moduledoc """
+  Documentation for `SimhashBench`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SimhashBench.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/simhash_bench_test.exs
@@ -0,0 +1,8 @@
+defmodule SimhashBenchTest do
+  use ExUnit.Case
+  doctest SimhashBench
+
+  test "greets the world" do
+    assert SimhashBench.hello() == :world
+  end
+end
